ef0dd7a939bf97b35d66124e3260eaf1a195f581
Rani <ranitatireddy@gmail.com>
2019-12-23 22:20:28 +0530

    First commit

---
+++ src/test/java/io/springsecurity/springsecuritycustomlogin/SpringsecurityCustomLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springsecurity.springsecuritycustomlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityCustomLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
